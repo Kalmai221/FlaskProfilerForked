67789b8e8bca9aa4517a1688cab9c2428d3f2da2
Kalmai221 Plays <67641154+Kalmai221@users.noreply.github.com>
2024-10-10 17:12:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ install_requires = [
 
 setup(
     name='Flask-ProfilerForked',
-    version='1.8.1.5',
+    version='1.8.1.6',
     url='https://github.com/Kalmai221/flask-profiler',
     license=open('LICENSE').read(),
     author='Kalmai221',
